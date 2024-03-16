a27d5f6ed8756c4d4b5094354343faf8a5affc26
lucamuscarnera <48683941+lucamuscarnera@users.noreply.github.com>
2024-03-16 23:04:33 +0100

    alcune descrizioni dei progetti

--- prettylist.js
+++ prettylist.js
@@ -3,8 +3,8 @@ prettylist.js:
 gestisce classi css di tipo lista creando l'animazione quando si clicca
 */
 
-window.onload = function()
-{
+	//while(MathJax == undefined);
+	//alert(MathJax)
 	var prettylists = document.getElementsByClassName("list");
 	for(var i = 0;i < prettylists.length;i++)
 	{
@@ -14,10 +14,36 @@ window.onload = function()
 		{
 			// per ogni item
 			var item = prettylist.children[j]
+			item.setAttribute('preview',prettylist.getAttribute('data'))
+
 			item.onmouseover = function()
 			{
 				this.style.backgroundColor = 'black';
 				this.style.color = 'white';
+			}
+			item.onmousedown = async function()
+			{
+				var preview_id = this.getAttribute('preview')
+				var preview = document.getElementById(preview_id);
+
+				preview.getElementsByClassName('preview_link')[0].innerHTML = "<a href='"+ this.getAttribute('link') + "'>"+this.innerText+"</a>";
+				var descrizione = preview.getElementsByClassName('preview_description')[0]
+				descrizione.innerText = "\n " +  this.getAttribute('data');
+				
+				// codice brutto
+				const mathDiv = descrizione;
+				await MathJax.startup.promise 			// make sure initial typesetting has taken place
+				MathJax.typesetClear([mathDiv]) 		// clear MathJax awareness of this element
+				await MathJax.typesetPromise([mathDiv]) // typeset anew
+				
+				var lista      = this.parentElement;
+				for(var k = 0 ; k < lista.children.length;k++)
+				{
+					var fratello = lista.children[k]
+					fratello.style.fontWeight = 'normal'
+				}
+					this.style.fontWeight = 'bold'
+				
 			}
 			item.onmouseout = function()
 			{
@@ -26,4 +52,4 @@ window.onload = function()
 			}
 		}
 	}
-}
+	
